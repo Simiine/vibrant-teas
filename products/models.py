2262bfabd93c86b34cc5b68b0193d51eb7c53fa3
Simi <simiinemboj@gmail.com>
2022-12-28 17:11:34 +0000

    create class for product model  in products models.py

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     """
@@ -32,3 +33,25 @@ class Subcategory(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+class Product(models.Model):
+    """
+    Class for the product model
+    """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=254)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=False)
+    description = models.TextField()
+    weight = models.IntegerField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
