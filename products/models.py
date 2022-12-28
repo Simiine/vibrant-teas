abdf0bc68e286eb75cd9807bd19e2ec3905ae3bd
Simi <simiinemboj@gmail.com>
2022-12-28 17:13:38 +0000

    create class for accessories model in products models.py

--- products/models.py
+++ products/models.py
@@ -55,3 +55,24 @@ class Product(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
+
+class Accessories(models.Model):
+    """
+    Class for the accessories model
+    """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
