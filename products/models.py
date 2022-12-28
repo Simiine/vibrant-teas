805f589549637c727ec2d9ae08170551b71351d2
Simi <simiinemboj@gmail.com>
2022-12-28 17:30:16 +0000

    comment out review class and producer under product class in products models.py

--- products/models.py
+++ products/models.py
@@ -39,7 +39,7 @@ class Product(models.Model):
     """
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
-    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
+    # producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
     name = models.CharField(max_length=254)
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=False)
     description = models.TextField()
@@ -77,24 +77,24 @@ class Accessories(models.Model):
             self.slug = slugify(self.name)
         return super().save(*args, **kwargs)
 
-class Review(models.Model):
-    """
-    Model for product review
-    """
-    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE, related_name="reviews")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_reviews')
-    review = models.TextField(max_length=1000)
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
+# class Review(models.Model):
+#     """
+#     Model for product review
+#     """
+#     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE, related_name="reviews")
+#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_reviews')
+#     review = models.TextField(max_length=1000)
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     approved = models.BooleanField(default=False)
     
-    class Meta:
-        """
-        Order reviews in ascending order
-        """
-        ordering = ['created_on']
+#     class Meta:
+#         """
+#         Order reviews in ascending order
+#         """
+#         ordering = ['created_on']
 
-    def __str__(self):
-        """
-        Return string representation
-        """
-        return f"Review {self.review} by {self.author}"
+#     def __str__(self):
+#         """
+#         Return string representation
+#         """
+#         return f"Review {self.review} by {self.author}"
