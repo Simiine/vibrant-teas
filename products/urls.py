fed16e344810a3bb4832c65238a131ddeb1563fc
Simi <simiinemboj@gmail.com>
2023-01-03 17:28:24 +0000

    Add accessories detail url

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
+    path('<accessories_id>', views.accessories_detail, name='accessories_detail'),
 ]
