88d2a931e7aa3fe1b06d0722a9bc146fa4f1d58a
Simi <simiinemboj@gmail.com>
2023-01-04 22:04:57 +0000

    Add adjust bag and remove bag to views.py and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -11,10 +11,6 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     weight = None
-    if 'product_weight' in request.POST:
-        weight = request.POST['product_weight']
-    bag = request.session.get('bag', {})
-
     if 'product_weight' in request.POST:
         weight = request.POST['product_weight']
     bag = request.session.get('bag', {})
@@ -34,5 +30,51 @@ def add_to_bag(request, item_id):
             bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    weight = None
+    if 'product_weight' in request.POST:
+        weight = request.POST['product_weight']
+    bag = request.session.get('bag', {})
+
+    if weight:
+        if quantity > 0:
+            bag[item_id]['items_by_weight'][weight] = quantity
+        else:
+            del bag[item_id]['items_by_weight'][weight]
+            if not bag[item_id]['items_by_weight']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        weight = None
+        if 'product_weight' in request.POST:
+            weight = request.POST['product_weight']
+        bag = request.session.get('bag', {})
+
+        if weight:
+            del bag[item_id]['items_by_weight'][weight]
+            if not bag[item_id]['items_by_weight']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
