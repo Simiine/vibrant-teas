61b6d47a3fca62baefac42d73f6d4d22090574b7
Simi <simiinemboj@gmail.com>
2023-01-06 23:19:33 +0000

    Fix webhook handler by importing stripe and adding variables to env.py and linking in settings.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,6 +5,7 @@ from products.models import Product
 
 import json
 import time
+import stripe
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -30,11 +31,13 @@ class StripeWH_Handler:
         save_info = intent.metadata.save_info
 
         # Get the Charge object
-        stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
 
-        billing_details = stripe_charge.billing_details # updated
+        billing_details = stripe_charge.billing_details  # updated
         shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2) # updated
+        grand_total = round(stripe_charge.amount / 100, 2)  # updated    
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
