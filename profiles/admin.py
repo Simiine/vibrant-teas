7ed844af5f235e15a02fe9a33a7241643e5ae1c9
Simi <simiinemboj@gmail.com>
2023-01-07 11:22:27 +0000

    Create admin view for profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin settings to display list of User Profile
+    """
+
+    model = UserProfile
+    list_display = (
+        'user',
+        'default_full_name',
+        'default_phone_number',
+        'default_street_address1',
+        'default_street_address2',
+        'default_town_or_city',
+        'default_county',
+        'default_postcode',
+        'default_country',
+    )
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
