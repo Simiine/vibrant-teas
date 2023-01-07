631322af185628bde530554d29f79bf5e4bf0fdb
Simi <simiinemboj@gmail.com>
2023-01-07 11:21:13 +0000

    Edit producers admin

--- producers/admin.py
+++ producers/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Producer
 
 class ProducerAdmin(admin.ModelAdmin):
+    """
+    Admin settings to display list of Producers
+    """
+
+    model = Producer
     list_display = ("name", "location")
     ordering = ("location",)
 
