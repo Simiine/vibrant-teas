a04d837714b33e624f5c70a900f93f69819220cf
Simi <simiinemboj@gmail.com>
2023-01-07 00:52:01 +0000

    Create add product view, url and template

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
-    # path('<accessories_id>', views.accessories_detail, name='accessories_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
