223498d3cbebb206f9fd379650a7b45adcaf7d6b
Simi <simiinemboj@gmail.com>
2023-01-15 12:18:27 +0000

    Deploy app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vibrant_teas.wsgi:application
--- vibrant_teas/settings.py
+++ vibrant_teas/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-^6@96f$1t&l(g6jje%cx(5^i8(pjf_#5#zax#3x7#ruz&ohvv0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vibrant-teas.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +126,17 @@ WSGI_APPLICATION = 'vibrant_teas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
