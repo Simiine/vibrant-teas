8919843b48b291cf9b8bd2c947accd4efb0a49b5
Simi <simiinemboj@gmail.com>
2023-01-21 01:24:44 +0000

    Delete error handlers

--- vibrant_teas/urls.py
+++ vibrant_teas/urls.py
@@ -17,10 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# from .views import handler404
 
-# Customer error page handlers
-# handler404 = 'vibrant_teas.views.handler404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
