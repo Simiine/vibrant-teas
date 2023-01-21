b4d30f84d515447398e2c5aee88febf4c4b8c809
Simi <simiinemboj@gmail.com>
2023-01-21 01:12:27 +0000

    Edit 404 page

--- vibrant_teas/urls.py
+++ vibrant_teas/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+# from .views import handler404
+
+# Customer error page handlers
+# handler404 = 'vibrant_teas.views.handler404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,7 +33,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
     path('contact/', include('contact.urls')),
-    # path('testimonials/', include('testimonials.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-handler404 = 'vibrant_teas.views.handler404'
