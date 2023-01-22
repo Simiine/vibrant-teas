5710a551673a2f1ff21c13c85cf1d46eb356eba3
Simi <simiinemboj@gmail.com>
2023-01-22 17:08:30 +0000

    Add comment to widgets py file

--- products/widgets.py
+++ products/widgets.py
@@ -3,6 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
+    """ 
+    Class that inherits the built-in ClearableFileInput.
+    """
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
