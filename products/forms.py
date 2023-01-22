72d0d90442f4e8bafd2006a9a07fc43df4024dd8
Simi <simiinemboj@gmail.com>
2023-01-22 16:28:09 +0000

    Change products forms include prefil subcategory in form and edit fields to not show slug in form

--- products/forms.py
+++ products/forms.py
@@ -1,22 +1,29 @@
 from django import forms
 from .widgets import CustomClearableFileInput
-from .models import Product, Category
+from .models import Product, Category, Subcategory
 
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('category', 'subcategory', 'sku', 'producer',
+                  'name', 'description', 'weight', 'price',
+                  'image_url', 'image')
 
     image = forms.ImageField(
         label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-        self.fields['category'].choices = friendly_names
+        categories = Category.objects.all()
+        friendly_names_cat = [(c.id, c.get_friendly_name()) for c in categories]
+
+        subcategories = Subcategory.objects.all()
+        friendly_names_sub = [(s.id, s.get_friendly_name()) for s in subcategories]
+
+        self.fields['category'].choices = friendly_names_cat
+        self.fields['subcategory'].choices = friendly_names_sub
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
