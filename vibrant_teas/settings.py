ad8cbbb249b3bf11c30383871fffa4210db42bb1
Simi <simiinemboj@gmail.com>
2023-01-23 09:29:46 +0000

    Delete debug = true from settings.py

--- vibrant_teas/settings.py
+++ vibrant_teas/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
 
 ALLOWED_HOSTS = ['vibrant-teas.herokuapp.com', 'localhost']
 
