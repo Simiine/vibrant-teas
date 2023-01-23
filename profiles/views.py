f39427755c1d5cd54c192671ef032eb201bb82b6
Simi <simiinemboj@gmail.com>
2023-01-23 04:47:38 +0000

    Fix profile view to update delivery details in user profile

--- profiles/views.py
+++ profiles/views.py
@@ -22,19 +22,20 @@ def profile(request):
                            'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
-        orders = profile.orders.all()
+    orders = profile.orders.all()
 
-        template = 'profiles/profile.html'
-        context = {
-            'form': form,
-            'orders': orders,
-            'on_profile_page': True
-        }
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
 
-        return render(request, template, context)
+    return render(request, template, context)
 
 
 def order_history(request, order_number):
+    """ Display the order history """
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
